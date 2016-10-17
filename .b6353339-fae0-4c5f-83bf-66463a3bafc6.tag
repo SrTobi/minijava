48cd4b3ab0df6d7e3d3237de22655a39f20a029f
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-10-18 00:00:53 +0200

    Added the required `build` and `run` scripts.
    Because the script must be named `build`, we cannot also have a
    `build` directory so I've renamed it to `bld`.
    
    	* .b6353339-fae0-4c5f-83bf-66463a3bafc6.tag: New.
    	* .gitignore: Don't ignore `/build` but do ignore `/bld/` and
    	also `*~`.
    	* build: New.
    	* run: New.
    	* src/sample.cpp: Make it print its command-line arguments so it
    	can be verified that `run` passes them through correctly.

---
+++ .b6353339-fae0-4c5f-83bf-66463a3bafc6.tag
@@ -0,0 +1,2 @@
+This file only exists so that we can verify that we are in the correct
+directory.  Its content is not important.
--- src/sample.cpp
+++ src/sample.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 
 
-
 #ifndef MINIJAVA_TEST
 
-int main() {
-    std::cout << "Hello Compiler!" << std::endl;
-    return 0;
+int main(int argc, char * * argv) {
+	std::cout << "Hello Compiler!" << std::endl;
+	for (auto i = 0; i < argc; ++i) {
+		std::cout << "argv[" << i << "] = " << argv[i] << "\n";
+	}
+	return 0;
 }
 
 #endif
