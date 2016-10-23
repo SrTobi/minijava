4d4adb555e709d08d7797b840cd2bb4be893cfdb
Moritz Baumann <codemonkey@landkeks.de>
2016-10-23 23:36:41 +0200

    Enabled additional diagnostics (conversion warnings).

--- extras/cmake/enable_diagnostics.cmake
+++ extras/cmake/enable_diagnostics.cmake
@@ -1,8 +1,9 @@
 macro(buildx_enable_diagnostics)
 	if(${CMAKE_C_COMPILER_ID} MATCHES "GNU|Clang")
-		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Werror -pedantic")
+		# -Wsign-conversion might be useful as well, but is too much of a PITA
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wconversion -Werror -pedantic")
 	endif()
 	if(${CMAKE_CXX_COMPILER_ID} MATCHES "GNU|Clang")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Werror -pedantic")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Werror -pedantic")
 	endif()
 endmacro(buildx_enable_diagnostics)
