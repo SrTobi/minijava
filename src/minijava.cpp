548e5363dda94293b94ea792b96331d88e8a55c7
Moritz Baumann <codemonkey@landkeks.de>
2016-10-25 16:55:47 +0200

    Never throw exceptions if a write to stderr fails.
    See discussion on BitBucket for the rationale.

--- src/minijava.cpp
+++ src/minijava.cpp
@@ -13,11 +13,9 @@ int main(int argc, char * * argv)
 		const auto args = std::vector<const char *>{argv, argv + argc};
 		std::cin.exceptions(std::iostream::badbit);
 		std::cout.exceptions(std::iostream::badbit);
-		std::cerr.exceptions(std::iostream::badbit);
 		minijava::real_main(args, std::cout, std::cerr);
 		return EXIT_SUCCESS;
 	} catch (const std::exception& e) {
-		std::cerr.exceptions(std::iostream::goodbit); // don't throw if caught exception cannot be written to stderr
 		std::cerr << MINIJAVA_PROJECT_NAME << ": error: " << e.what() << "\n";
 		return EXIT_FAILURE;
 	}
