28ed9d1d01c7176a10a2c9761d58f16610e2b9d9
SrTobi <code.databyte@gmail.com>
2016-10-26 01:42:38 +0200

    new methods making symbol more std::string conform

--- src/symbol.hpp
+++ src/symbol.hpp
@@ -74,6 +74,18 @@ namespace minijava
 		}
 
 	public:
+		using value_type      = char;
+		using traits_type     = std::char_traits<char>;
+		using const_reference = const char&;
+		using reference       = const_reference;
+		using const_pointer   = const char *;
+		using pointer         = const_pointer;
+		using const_iterator  = const_pointer;
+		using iterator        = const_iterator;
+		using difference_type = std::ptrdiff_t;
+		using size_type       = std::size_t;
+
+	public:
 
 		/**
 		 * @brief
@@ -88,7 +100,225 @@ namespace minijava
 		{
 			return _entry->cstr;
 		}
-		
+
+		/**
+		 * @brief
+		 *     Returns the length of the string, in terms of bytes.
+		 *
+		 * Note that string objects handle bytes without knowledge of the encoding
+		 * that may eventually be used to encode the characters it contains.
+		 * Therefore, the value returned may not correspond to the actual number of
+		 * encoded characters in sequences of multi-byte or variable-length characters (such as UTF-8).
+		 *
+		 * Both symbol::size and symbol::length are synonyms and return the same value.
+		 *
+		 * @returns
+		 *     The number of bytes in the string.
+		 *
+		 */
+		constexpr size_type size() const noexcept
+		{
+			return _entry->size;
+		}
+
+		/**
+		 * @brief
+		 *     Returns the length of the string, in terms of bytes.
+		 *
+		 * Note that string objects handle bytes without knowledge of the encoding
+		 * that may eventually be used to encode the characters it contains.
+		 * Therefore, the value returned may not correspond to the actual number of
+		 * encoded characters in sequences of multi-byte or variable-length characters (such as UTF-8).
+		 *
+		 * Both symbol::size and symbol::length are synonyms and return the same value.
+		 *
+		 * @returns
+		 *     The number of bytes in the string.
+		 *
+		 */
+		constexpr size_type length() const noexcept
+		{
+			return size();
+		}
+
+		/**
+		 * @brief
+		 *     Get C string equivalent.
+		 *
+		 * Returns a pointer to an array that contains a null-terminated sequence
+		 * of characters (i.e., a C-string) representing the value of the symbol.
+		 *
+		 * This array includes the same sequence of characters that make up the
+		 * value of the symbol plus an additional terminating null-character ('\0') at the end.
+		 *
+		 * @returns
+		 *     A pointer to the c-string representation of the symbol's value.
+		 *
+		 */
+		constexpr const char * data() const noexcept
+		{
+			return _entry->cstr;
+		}
+
+		/**
+		 * @brief
+		 *     Return iterator to beginning
+		 *
+		 * Returns an iterator pointing to the first character of the symbol.
+		 *
+		 * @returns
+		 *     An iterator to the beginning of the symbol.
+		 *
+		 */
+		constexpr const_iterator begin() const noexcept
+		{
+			return cbegin();
+		}
+
+		/**
+		 * @brief
+		 *     Return iterator to end
+		 *
+		 * Returns an iterator pointing to the past-the-end character of the symbol.
+		 *
+		 * @returns
+		 *     An iterator to the beginning of the symbol.
+		 *
+		 */
+		constexpr const_iterator end() const noexcept
+		{
+			return cend();
+		}
+
+		/**
+		 * @brief
+		 *     Return iterator to beginning
+		 *
+		 * Returns an iterator pointing to the first character of the symbol.
+		 *
+		 * @returns
+		 *     An iterator to the beginning of the symbol.
+		 *
+		 */
+		constexpr const_iterator cbegin() const noexcept
+		{
+			return _entry->cstr;
+		}
+
+		/**
+		 * @brief
+		 *     Return iterator to end
+		 *
+		 * Returns an iterator pointing to the past-the-end character of the symbol.
+		 *
+		 * @returns
+		 *     An iterator to the beginning of the symbol.
+		 *
+		 */
+		constexpr const_iterator cend() const noexcept
+		{
+			return cbegin() + size();
+		}
+
+		/**
+		 * @brief
+		 *     Test if symbol is empty.
+		 *
+		 * Returns whether the symbol is the empty symbol (i.e. whether its length is 0).
+		 *
+		 * @returns
+		 *     `true` if the symbol length is 0, `false` otherwise.
+		 *
+		 */
+		constexpr bool empty() const noexcept
+		{
+			return _entry->size == 0;
+		}
+
+		/**
+		 * @brief
+		 *     Get character in symbol.
+		 *
+		 * Returns the character at position `pos` in the symbol.
+		 *
+		 * If `pos` is less or equal to the symbols length,
+		 * the function never throws exceptions (no-throw guarantee).
+		 * Otherwise, it causes undefined behavior.
+		 *
+		 * @param pos
+		 *     Value with the position of a character within the symbol.
+		 *
+		 * @returns
+		 *     The character at the specified position in the symbol.
+		 *
+		 */
+		constexpr char operator[](std::size_t pos) const
+		{
+			assert(pos < size());
+			return _entry->cstr[pos];
+		}
+
+		/**
+		 * @brief
+		 *     Get character in symbol.
+		 *
+		 * Returns the character at position `pos` in the symbol.
+		 *
+		 * The function automatically checks whether `pos` is a valid
+		 * position of a character in the string (i.e., whether `pos` is less than the symbol length),
+		 * throwing an out_of_range exception if it is not.
+		 *
+		 * @param pos
+		 *     Value with the position of a character within the symbol.
+		 *
+		 * @returns
+		 *     The character at the specified position in the symbol.
+		 *
+		 * @throws std::out_of_range
+		 *     If `pos` is not less than the symbol's length.
+		 *
+		 */
+		constexpr char at(std::size_t pos) const
+		{
+			if(pos >= size())
+				throw std::out_of_range("invalid position in symbol");
+			return _entry->cstr[pos];
+		}
+
+		/**
+		 * @brief
+		 *     Returns the first character.
+		 *
+		 * This function shall not be called on the empty symbol.
+		 * Doing so causes undefined behavior.
+		 *
+		 * @returns
+		 *     The first charactor of the symbol.
+		 *
+		 */
+		constexpr char front() const
+		{
+			assert(size() > 0);
+			return *cbegin();
+		}
+
+		/**
+		 * @brief
+		 *     Returns the last character.
+		 *
+		 * This function shall not be called on the empty symbol.
+		 * Doing so causes undefined behavior.
+		 *
+		 * @returns
+		 *     The last charactor of the symbol.
+		 *
+		 */
+		constexpr char back() const
+		{
+			assert(size() > 0);
+			return *(cend() - 1);
+		}
+
 		/**
 		* @brief
 		*     Compares two `string`s for equality.
