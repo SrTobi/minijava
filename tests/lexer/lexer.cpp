bb7f5e4013e7f7b02f7aa18d9c75191babd60044
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-10-26 15:59:31 +0200

    Scratch implementation of "nice" lexer test.
    	* tests/lexer/lexer.cpp: See for yourself.

--- tests/lexer/lexer.cpp
+++ tests/lexer/lexer.cpp
@@ -2,13 +2,85 @@
 
 #include <algorithm>
 #include <string>
+#include <iostream>
 
 #define BOOST_TEST_MODULE  lexer_lexer
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 
 #include "lexer/token.hpp"
 #include "lexer/token_iterator.hpp"
 #include "symbol_pool.hpp"
+
+
+namespace /* anonymous */
+{
+
+	struct success_test_data
+	{
+		std::string input{};
+		minijava::symbol_pool<> pool{};
+		std::vector<minijava::token> expected{};
+	};
+
+	class success_test
+	{
+	public:
+
+		template <typename... ArgTs>
+		success_test(const char * input, ArgTs&&... args)
+		{
+			_input = input;
+			_expected = {_make_expected_token(std::forward<ArgTs>(args))...,
+						 minijava::token::create(minijava::token_type::eof)};
+		}
+
+		success_test_data get() const
+		{
+			auto sample = success_test_data{};
+			sample.input = _input;
+			for (const auto& t : _expected) {
+				auto copy = t;
+				if (t.type() == minijava::token_type::identifier) {
+					auto canon = sample.pool.normalize(t.name().c_str());
+					copy = minijava::token::create_identifier(canon);
+				}
+				sample.expected.push_back(std::move(copy));
+			}
+			return sample;
+		}
+
+	private:
+
+		auto _make_expected_token(const char *const name)
+		{
+			const auto canon = _pool.normalize(name);
+			return minijava::token::create_identifier(canon);
+		}
+
+		auto _make_expected_token(const std::uint32_t value)
+		{
+			return minijava::token::create_integer_literal(value);
+		}
+
+		auto _make_expected_token(const minijava::token_type tt)
+		{
+			return minijava::token::create(tt);
+		}
+
+		minijava::symbol_pool<> _pool{};
+		std::string _input{};
+		std::vector<minijava::token> _expected{};
+
+		friend std::ostream& operator<<(std::ostream& os, const success_test& test)
+		{
+			return os << test._input;
+		}
+
+	};
+
+}  // namespace /* anonymous */
+
 
 
 BOOST_AUTO_TEST_CASE(empty_input_starts_with_eof_and_stays_there)
@@ -41,5 +113,20 @@ BOOST_AUTO_TEST_CASE(sequence_of_identifiers_lexed_correctly)
 							 minijava::token_begin(lex), minijava::token_end(lex)));
 }
 
+
+static const success_test success_data[] = {
+	{""},
+	{"alpha", "alpha"},
+	{"alpha beta", "alpha", "beta"},
+};
+
+BOOST_DATA_TEST_CASE(success, success_data)
+{
+	auto s = sample.get();
+	auto lex = minijava::make_lexer(std::begin(s.input), std::end(s.input), s.pool);
+	BOOST_REQUIRE(std::equal(std::begin(s.expected), std::end(s.expected),
+							 minijava::token_begin(lex), minijava::token_end(lex)));
+
+}
 
 // TODO @Moritz Baumann: Beef this file up with thorough unit tests.
