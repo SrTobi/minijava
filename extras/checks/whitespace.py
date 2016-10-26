36a7fcbb5a4fc33132965eb192ab4beb2917bf30
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-10-26 04:56:30 +0200

    Also check vertical white-space.
    I thought this would be an easy exercise but it turned out I had to do some
    major refactoring.  We're now reading the whole file into memory upfront (which
    should be okay in this case).  The code was also cleaned up during the
    refactoring and should now be easier to extend.
    
    	* extras/checks/whitespace.py: Enhance and refactor.

--- extras/checks/whitespace.py
+++ extras/checks/whitespace.py
@@ -3,8 +3,8 @@
 
 
 import argparse
+import itertools as it
 import os
-import shutil
 import subprocess
 import sys
 
@@ -20,270 +20,6 @@ class StyleError(Exception):
 	"""
 
 	pass
-
-
-class NullContext(object):
-
-	"""
-	A context manager that manages nothing.
-
-	"""
-
-	def __enter__(self):
-		return self
-
-	def __exit__(self, exec_type, exec_val, exec_tb):
-		pass
-
-
-def maybe_open(filename, mode='r'):
-	"""
-	Like the builtin `open` function except that the `filename` may be `None`
-	in which case a `NullContext` is `returned.
-
-	@param filename : str | NoneType
-		name of the file to open or `None`
-
-	@param mode : str
-		file-open mode (forwarded to `open`)
-
-	@returns file-like | NullContext
-		open file or `NullContext`
-
-	@raises OSError
-		if `raise`d by `open`
-
-	"""
-	if filename is None:
-		return NullContext()
-	else:
-		return open(filename, mode)
-
-
-def make_backup_file_name(filename):
-	"""
-	Given a file name, `return`s the file name for the corresponding backup
-	file.
-
-	@param filename : str
-		file name for the original file
-
-	@returns str
-		file name for the backup file
-
-	@raises ValueError
-		if the file name already looks like a backup file name
-
-	"""
-	backup_suffix = '~' if os.name == 'posix' else '.backup'
-	if filename.endswith(backup_suffix):
-		raise ValueError("Looks like a backup file: " + filename)
-	return filename + backup_suffix
-
-
-def is_regular_file(filename):
-	"""
-	Checks whether `filename` corresponds to a regular file and is not a
-	symbolic link.
-
-	@param filename : str
-		file name to check
-
-	@returns bool
-		whether `filename` is a regular file
-
-	"""
-	return os.path.isfile(filename) and not os.path.islink(filename)
-
-
-def git_ls_files():
-	"""
-	`return`s a list of of version-controlled files as reported by
-	`git ls-files` filtered to contain only regular files.
-
-	@returns [str]
-		list of file names
-
-	@raises OSError
-		if running Git fails
-
-	"""
-	proc = subprocess.Popen(
-		['git', 'ls-files'],
-		stdin=subprocess.DEVNULL,
-		stdout=subprocess.PIPE,
-		stderr=None
-	)
-	(stdout, stderr) = proc.communicate()
-	if proc.returncode != 0:
-		raise OSError("Cannot list version-controlled files")
-	filenames = stdout.decode().split()
-	return list(filter(is_regular_file, filenames))
-
-
-def check_line(line):
-	"""
-	Checks a single line for conformance with the style guide.
-
-	@param line : str
-		line to check
-
-	@raises StyleError
-		if the line is non-conforming
-
-	"""
-	if not line.endswith('\n'):
-		raise StyleError("No new-line character at end of line")
-	line = line[:-1]
-	if line.rstrip() != line:
-		raise StyleError("Line has trailing white-space")
-	if '\t' in line.lstrip('\t'):
-		raise StyleError("Tabs may only be used for indentation")
-
-
-def untabify(line, tabwidth):
-	"""
-	Expands all but leading tabs to spaces.
-
-	@param line : str
-		line to untabify
-
-	@param tabwidth : int
-		characters per tab
-
-	@returns str
-		untabified line
-
-	"""
-	assert tabwidth > 0
-	body = list()
-	i = 0
-	while i < len(line) and line[i] == '\t':
-		i += 1
-	indent = '\t' * i
-	while i < len(line):
-		if line[i] == '\t':
-			skip = tabwidth - (len(body) % tabwidth)
-			body.extend(' ' for i in range(skip))
-		else:
-			body.append(line[i])
-		i += 1
-	return indent + ''.join(body)
-
-
-def fix_line(line):
-	"""
-	Fixes some style guide violations in a line.
-
-	For pretty obvious reasons, this function cannot fix all violations and has
-	to apply conservative heuristics for the ones it can fix.  In particular,
-	`check_line(fix_line(text) + '\n')` totally can fail.
-
-	@param line : str
-		line to fix
-
-	@returns str
-		fixed line without trailing new-line character
-
-	"""
-	line = line.rstrip()
-	line = untabify(line, default_tab_width)
-	return line
-
-
-def check_file(filename, fix, verboseout):
-	"""
-	Checks a single file for style guide violations and optionally fixes (some
-	of) them in-place.
-
-	@param filename : str
-		name of the file
-
-	@param fix : bool
-		whether to fix violations inline
-
-	@param verboseout : file-like
-		terminal to print violations to
-
-	@returns : int
-		number of violations found
-
-	"""
-	violations = 0
-	infilename = filename
-	if fix:
-		infilename = make_backup_file_name(filename)
-		shutil.copy(filename, infilename, follow_symlinks=False)
-	with maybe_open(filename if fix else None, 'w') as ostr:
-		with open(infilename, 'r') as istr:
-			for (i, line) in enumerate(istr):
-				candidate = line
-				if fix:
-					candidate = fix_line(line) + '\n'
-					print(candidate, end='', file=ostr)
-				try:
-					check_line(candidate)
-				except StyleError as e:
-					violations += 1
-					msg = "{:s}:{:d}: {:s}".format(filename, i + 1, str(e))
-					print(msg, file=verboseout)
-	return violations
-
-
-def check_files(filenames, fix, verboseout, summaryout):
-	"""
-	Checks zero or more files for style guide violations and optionally fixes
-	them in-place.
-
-	@param filenames : [str]
-		file names of the files to check
-
-	@param fix : bool
-		whether to fix violations inline
-
-	@param verboseout : file-like
-		terminal to print per-line violations to
-
-	@param summaryout : file-like
-		terminal to print per-file violations to
-
-	@returns : int
-		number of non-conforming files
-
-	"""
-	for fn in filenames:
-		if not is_regular_file(fn):
-			raise ValueError("Not a regular file: " + fn)
-	bad_files = 0
-	for fn in filenames:
-		violations = check_file(fn, fix, verboseout)
-		if violations > 0:
-			bad_files += 1
-			msg = "{:s}: {:d} violations found".format(fn, violations)
-			print(msg, file=summaryout)
-		else:
-			print("{:s}: ok".format(fn), file=summaryout)
-	return bad_files
-
-
-def real_file_name(filename):
-	"""
-	`return`s its argument unchanged except when it is `-` in which case a
-	`ValueError` is `raise`d.
-
-	@param filename : str
-		file name to check
-
-	@returns str
-		filename
-
-	@raises ValueError
-		if `filename` is `-`
-
-	"""
-	if filename == '-':
-		raise ValueError("Not a valid file name: " + filename)
-	return filename
 
 
 def main(args):
@@ -322,7 +58,7 @@ def main(args):
 	)
 	ap.add_argument(
 		'filenames', metavar='FILE', nargs='*',
-		type=real_file_name, help="regular file to check"
+		type=reasonable_file_name, help="regular file to check"
 	)
 	ap.add_argument(
 		'-s', '--summary', action='store_true',
@@ -343,6 +79,8 @@ def main(args):
 	ns = ap.parse_args(args)
 	if ns.directory is not None:
 		os.chdir(ns.directory)
+	for fn in it.filterfalse(is_regular_file, ns.filenames):
+		raise ValueError("Not a regular file: {:s}".format(fn))
 	with open(os.devnull, 'a') as devnull:
 		return check_files(
 			ns.filenames if ns.filenames else git_ls_files(),
@@ -350,6 +88,304 @@ def main(args):
 			sys.stderr if not ns.quiet and not ns.summary else devnull,
 			sys.stderr if not ns.quiet and     ns.summary else devnull,
 		)
+
+
+def check_files(filenames, fix, verboseout, summaryout):
+	"""
+	Checks zero or more files for style guide violations and optionally fixes
+	them in-place.
+
+	@param filenames : [str]
+		file names of the files to check
+
+	@param fix : bool
+		whether to fix violations inline
+
+	@param verboseout : file-like
+		terminal to print per-line violations to
+
+	@param summaryout : file-like
+		terminal to print per-file violations to
+
+	@returns : int
+		number of non-conforming files
+
+	"""
+	okmsg = "no issues found" if not fix else "fixed"
+	badmsg = "issues found"
+	bad_files = 0
+	for fn in filenames:
+		lines = read_file_and_maybe_fix_it(fn, fix)
+		if check_content(fn, lines, verboseout):
+			print("{:s}: {}".format(fn, okmsg), file=summaryout)
+		else:
+			bad_files += 1
+			msg = "{:s}: {}".format(fn, badmsg)
+			print(msg, file=summaryout)
+	return bad_files
+
+
+def check_content(filename, lines, verboseout):
+	"""
+	Checks the entire content of a single file for style guide violations.
+
+	@param filename : str
+		name of the file (only used for diagnostic messages)
+
+	@param lines : [str]
+		contents (lines) of the file
+
+	@param fix : bool
+		whether to fix violations inline
+
+	@returns : bool
+		whether the file was conforming
+
+	"""
+	ok = True
+	try:
+		check_vertical(lines)
+	except StyleError as e:
+		ok = False
+		msg = "{:s}: {:s}".format(filename, str(e))
+		print(msg, file=verboseout)
+	for (i, line) in enumerate(lines):
+		try:
+			check_horizontal(line)
+		except StyleError as e:
+			ok = False
+			msg = "{:s}:{:d}: {:s}".format(filename, i + 1, str(e))
+			print(msg, file=verboseout)
+	return ok
+
+
+def check_vertical(lines):
+	"""
+	Checks the lines of a file for vertical white-space issues.
+
+	@param lines : [str]
+		lines of the file
+
+	@raises StyleError
+		if vertical white-space issues are found
+
+	"""
+	first = lambda pair : pair[0]
+	second = lambda pair : pair[1]
+	count = lambda seq : sum(1 for x in seq)
+	filtergroup = lambda pred, key, seq : filter(pred, it.groupby(seq, key))
+	for blank in it.takewhile(is_blank, lines):
+		raise StyleError("Blank lines at beginning of file")
+	for blank in it.takewhile(is_blank, reversed(lines)):
+		raise StyleError("Blank lines at end of file")
+	if lines and not lines[-1].endswith('\n'):
+		raise StyleError("No new-line character at end of last line")
+	if max(map(count, map(second, filtergroup(first, is_blank, lines))), default=0) > 3:
+		raise StyleError("File contains excessive vertical white-space")
+
+
+def check_horizontal(line):
+	"""
+	Checks a single line of a file for horizontal white-space issues.
+
+	@param line : str
+		line to check (including trailing newline character)
+
+	@raises StyleError
+		if horizontal white-space issues are found
+
+	"""
+	line = line.rstrip('\n')
+	if line.rstrip() != line:
+		raise StyleError("Line has trailing white-space")
+	if '\t' in line.lstrip('\t'):
+		raise StyleError("Tabs may only be used for indentation")
+
+
+def fix_vertical(lines):
+	"""
+	Fixes some issues with vertical white-space in a line.
+
+	@param lines : [str]
+		lines of the file to fix
+
+	@returns [str]
+		fixed lines
+
+	"""
+	trim = lambda l : list(it.dropwhile(is_blank, l))
+	return trim(reversed(trim(reversed(lines))))
+
+
+def fix_horizontal(line):
+	"""
+	Fixes some issues with horizontal white-space in a line.
+
+	@param line : str
+		line to fix
+
+	@returns str
+		fixed line with trailing new-line character
+
+	"""
+	line = line.rstrip()
+	line = untabify(line, default_tab_width)
+	return line + '\n'
+
+
+def is_blank(line):
+	"""
+	Tests whether a line contains only white-space.
+
+	@param line : str
+		line to test
+
+	@returns bool
+		whether the line consists entirely of white-space
+
+	"""
+	return not bool(line.strip())
+
+
+def untabify(line, tabwidth):
+	"""
+	Expands all but leading tabs to spaces.
+
+	@param line : str
+		line to untabify
+
+	@param tabwidth : int
+		characters per tab
+
+	@returns str
+		untabified line
+
+	"""
+	assert tabwidth > 0
+	body = list()
+	i = 0
+	while i < len(line) and line[i] == '\t':
+		i += 1
+	indent = '\t' * i
+	while i < len(line):
+		if line[i] == '\t':
+			skip = tabwidth - (len(body) % tabwidth)
+			body.extend(' ' for i in range(skip))
+		else:
+			body.append(line[i])
+		i += 1
+	return indent + ''.join(body)
+
+
+def read_file_and_maybe_fix_it(filename, fix):
+	"""
+	`return`s the lines of a file as a `list` and optionally fixes it in-place.
+
+	If `fix` is `False`, the function will merely read the file.  If `fix` is
+	`True`, it will read the file, fix the contents, rename the file to a
+	backup file, write back the fixed contents to the original file and finally
+	`return` the fixed contents.
+
+	@param filename : str
+		name of the file
+
+	@param fix : bool
+		whether to fix violations inline
+
+	@returns : [str]
+		optionally fixed lines of the file
+
+	"""
+	with open(filename, 'r') as istr:
+		lines = istr.readlines()
+	if fix:
+		lines = list(map(fix_horizontal, fix_vertical(lines)))
+		backupname = make_backup_file_name(filename)
+		os.replace(filename, backupname)
+		with open(filename, 'w') as ostr:
+			ostr.writelines(lines)
+	return lines
+
+
+def make_backup_file_name(filename):
+	"""
+	Given a file name, `return`s the file name for the corresponding backup
+	file.
+
+	@param filename : str
+		file name for the original file
+
+	@returns str
+		file name for the backup file
+
+	@raises ValueError
+		if the file name already looks like a backup file name
+
+	"""
+	backup_suffix = '~' if os.name == 'posix' else '.backup'
+	if filename.endswith(backup_suffix):
+		raise ValueError("Looks like a backup file: " + filename)
+	return filename + backup_suffix
+
+
+def git_ls_files():
+	"""
+	`return`s a list of of version-controlled files as reported by
+	`git ls-files` filtered to contain only regular files.
+
+	@returns [str]
+		list of file names
+
+	@raises OSError
+		if running Git fails
+
+	"""
+	proc = subprocess.Popen(
+		['git', 'ls-files'],
+		stdin=subprocess.DEVNULL,
+		stdout=subprocess.PIPE,
+		stderr=None
+	)
+	(stdout, stderr) = proc.communicate()
+	if proc.returncode != 0:
+		raise OSError("Cannot list version-controlled files")
+	filenames = stdout.decode().split()
+	return list(filter(is_regular_file, filenames))
+
+
+def is_regular_file(filename):
+	"""
+	Checks whether `filename` corresponds to a regular file and is not a
+	symbolic link.
+
+	@param filename : str
+		file name to check
+
+	@returns bool
+		whether `filename` is a regular file
+
+	"""
+	return os.path.isfile(filename) and not os.path.islink(filename)
+
+
+def reasonable_file_name(filename):
+	"""
+	`return`s its argument unchanged except when it looks like a reasonable
+	file name or otherwise `raise` a `ValueError`.
+
+	@param filename : str
+		file name to check
+
+	@returns str
+		filename
+
+	@raises ValueError
+		if `filename` does not look like a file name
+
+	"""
+	if filename == '-':
+		raise ValueError("Not a valid file name: " + filename)
+	return filename
 
 
 if __name__ == '__main__':
