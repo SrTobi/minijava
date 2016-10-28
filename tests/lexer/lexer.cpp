e0afe565be1721d1f39129bfe680b03c092da6a3
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-10-28 06:12:17 +0200

    A very basic unit test for the lexer.
    This test checks that each punctuation token is lexed correctly in isolation.
    It can also check the same for isolated keywords but since keywords are not
    implemented in the lexer yet, this is currently commented-out from the test.
    Un-comment the obvious to also check keywords.  The test uses a
    `std::forward_list` as input.  Invoking undefined behavior by dereferencing a
    past-the-end iterator for a `std::string` often goes by unnoticed because
    you're lucky to hit the NUL byte.  It is undefined behavior nonetheless.  Our
    actual compiler will invoke the lexer with input iterators which are even
    weaker then the forward iterators provided by `std::forward_list`.
    
    	* tests/lexer/lexer.cpp: Added one unit test.

--- tests/lexer/lexer.cpp
+++ tests/lexer/lexer.cpp
@@ -1,10 +1,12 @@
 #include "lexer/lexer.hpp"
 
 #include <algorithm>
+#include <forward_list>
 #include <string>
 
 #define BOOST_TEST_MODULE  lexer_lexer
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 
 #include "lexer/token.hpp"
 #include "lexer/token_iterator.hpp"
@@ -39,6 +41,32 @@ BOOST_AUTO_TEST_CASE(sequence_of_identifiers_lexed_correctly)
 	auto lex = minijava::make_lexer(std::begin(ids), std::end(ids), pool);
 	BOOST_REQUIRE(std::equal(std::begin(expected), std::end(expected),
 							 minijava::token_begin(lex), minijava::token_end(lex)));
+}
+
+
+static const auto single_token_data = minijava::all_token_types();
+
+BOOST_DATA_TEST_CASE(single_tokens_are_lexed_correctly, single_token_data)
+{
+	const auto punct = minijava::token_category::punctuation;
+	/* const auto kw = minijava::token_category::keyword; */
+	const auto cat = category(sample);
+	if ((cat == punct) /* || (cat == kw) */) {
+		const auto text = std::string{name(sample)};
+		const auto input = std::forward_list<char>{std::begin(text), std::end(text)};
+		auto pool = minijava::symbol_pool<>{};
+		try {
+			auto lex = minijava::make_lexer(std::begin(input), std::end(input), pool);
+			BOOST_REQUIRE_EQUAL(sample, lex.current_token().type());
+			lex.advance();
+			BOOST_REQUIRE(lex.current_token_is_eof());
+		} catch (const minijava::lexical_error& e) {
+			BOOST_FAIL(
+				 "Unexpected lexical error for input '" << text << "'"
+				 << " (" << text.length() << " characters): " << e.what()
+			);
+		}
+	}
 }
 
 
