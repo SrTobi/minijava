574ee20e9737422257bfc01f227d1eda60b63992
SrTobi <code.databyte@gmail.com>
2016-10-29 14:03:39 +0200

    added some symbol_pool tests

--- tests/symbol_pool.cpp
+++ tests/symbol_pool.cpp
@@ -18,6 +18,7 @@ BOOST_AUTO_TEST_CASE(empty_when_default_constructed)
 BOOST_AUTO_TEST_CASE(empty_pool_contains_nothing)
 {
 	const auto pool = minijava::symbol_pool<>{};
+	BOOST_REQUIRE(not pool.contains(""));
 	BOOST_REQUIRE(not pool.contains("elephant"));
 }
 
@@ -71,4 +72,45 @@ BOOST_AUTO_TEST_CASE(correct_size_after_normalization)
 	pool.normalize("beta");  // should already be present
 	BOOST_REQUIRE_EQUAL(std::size_t{4}, pool.size());
 	BOOST_REQUIRE(not pool.empty());
+}
+
+BOOST_AUTO_TEST_CASE(move_constructed_pool_behaves_like_old_pool)
+{
+	using namespace std::string_literals;
+	auto pool = minijava::symbol_pool<>{};
+	const auto text = "matchstick"s;
+	const auto canonical = pool.normalize(text);
+	BOOST_REQUIRE(canonical.c_str() != text.c_str());
+
+	auto moved_pool = std::move(pool);
+	const auto canonical_from_moved = moved_pool.normalize(text);
+	BOOST_REQUIRE_EQUAL(canonical_from_moved, canonical);
+	BOOST_REQUIRE_EQUAL(moved_pool.size(), std::size_t(1));
+
+	// old pool is empty
+	BOOST_REQUIRE(pool.empty());
+}
+
+BOOST_AUTO_TEST_CASE(move_assigned_pool_behaves_like_old_pool)
+{
+	using namespace std::string_literals;
+	auto pool = minijava::symbol_pool<>{};
+	const auto text = "matchstick"s;
+	const auto canonical = pool.normalize(text);
+	BOOST_REQUIRE(canonical.c_str() != text.c_str());
+	BOOST_REQUIRE_EQUAL(canonical.c_str(), text.c_str());
+
+	auto second_pool = minijava::symbol_pool<>{};
+
+	const auto canonical2 = second_pool.normalize(text);
+	BOOST_REQUIRE_EQUAL(canonical2.c_str(), canonical.c_str());
+	BOOST_REQUIRE(not canonical2.empty());
+
+	second_pool = std::move(pool);
+	const auto canonical_from_moved = second_pool.normalize(text);
+	BOOST_REQUIRE_EQUAL(canonical_from_moved, canonical);
+	BOOST_REQUIRE_EQUAL(second_pool.size(), std::size_t(1));
+
+	// old pool is empty
+	BOOST_REQUIRE(pool.empty());
 }
