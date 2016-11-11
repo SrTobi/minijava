f541dc6c46a035f2092092b8b1f2b31d5674d0e9
Moritz Baumann <moritz.baumann@student.kit.edu>
2016-11-12 00:04:43 +0100

    Fix doxygen warnings by adding documentation to pretty_printer stub.

--- src/parser/pretty_printer.hpp
+++ src/parser/pretty_printer.hpp
@@ -8,15 +8,28 @@ namespace minijava
 {
 	namespace ast
 	{
+		/**
+		 * AST pretty printer
+		 */
 		class pretty_printer : public visitor
 		{
 		public:
 
+			/**
+			 * @brief
+			 *     Constructs a new pretty printer which writes to the given
+			 *     output stream.
+			 *
+			 * @param output
+			 *     output stream
+			 *
+			 */
 			pretty_printer(std::ostream& output)
 					: _output{output} {}
 
 		private:
 
+			/** @brief output stream */
 			std::ostream& _output;
 
 			//std::size_t indentation_level;
