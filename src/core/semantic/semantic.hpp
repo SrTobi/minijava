7b32e9839678788f5827ec20d69c157f7a6611da
Moritz Baumann <moritz.baumann@student.kit.edu>
2016-11-22 20:17:29 +0100

    Started defining external interface of the semantic analysis.

--- src/core/semantic/semantic.hpp
+++ src/core/semantic/semantic.hpp
@@ -8,13 +8,138 @@
 
 #pragma once
 
+#include <cstdint>
+#include <unordered_set>
+
 #include "parser/ast.hpp"
+#include "semantic/attribute.hpp"
 #include "symbol/symbol_pool.hpp"
 
 
 namespace minijava
 {
-	// FIXME: return all data relevant to future stages
+
+	/**
+	 * @brief
+	 *     Container for all semantic information collected during the semantic
+	 *     analysis of the program.
+	 *
+	 * Among other information, this data structure contains the AST
+	 * annotations.
+	 *
+	 */
+	// FIXME: define constraints for attribute types
+	struct semantic_info
+	{
+		/**
+		 * @brief
+		 *     Type of the map containing all type definitions.
+		 */
+		// FIXME: map string -> type_info
+		using type_definitions = int;
+
+		/**
+		 * @brief
+		 *     Type of the type annotations on expressions and other AST nodes.
+		 */
+		// FIXME: type_info + rank data structure
+		using type_annotations = ast_attributes<int>;
+
+		/**
+		 * @brief
+		 *     Type of the local variable list annotations on method nodes.
+		 */
+		using locals = ast_attributes<std::unordered_set<const ast::var_decl*>>;
+
+		/**
+		 * @brief
+		 *     Type of the annotations pointing to variable declarations.
+		 */
+		using var_declarations = ast_attributes<const ast::var_decl*>;
+
+		/**
+		 * @brief
+		 *     Type of the annotations pointing to method declarations.
+		 */
+		using method_declarations = ast_attributes<const ast::instance_method*>;
+
+		/**
+		 * @brief
+		 *     Type of the constant value expression annotations.
+		 */
+		using constant_values = ast_attributes<std::int32_t>;
+
+		/**
+		 * @brief
+		 *     Constructs a semantic information container.
+		 *
+		 * @param types
+		 *     types in the program
+		 *
+		 * @param ast_types
+		 *     type annotations
+		 *
+		 * @param locals
+		 *     local variables annotations of methods
+		 *
+		 * @param var_decls
+		 *     variable declaration pointer annotations
+		 *
+		 * @param method_decls
+		 *     method declaration pointer annotations
+		 *
+		 * @param constants
+		 *     constant value annotations
+		 *
+		 */
+		semantic_info(type_definitions types, type_annotations ast_types,
+		              locals locals, var_declarations var_decls,
+		              method_declarations method_decls,
+					  constant_values constants)
+				: _types{std::move(types)}, _ast_types{std::move(ast_types)},
+				  _locals{std::move(locals)}, _var_decls{std::move(var_decls)},
+				  _method_decls{std::move(method_decls)},
+				  _constants{std::move(constants)}
+		{}
+
+		/** @brief types in the program **/
+		type_definitions _types;
+
+		/** @brief type annotations **/
+		type_annotations _ast_types;
+
+		/** @brief local variables annotations of methods **/
+		locals _locals;
+
+		/** @brief variable declaration pointer annotations **/
+		var_declarations _var_decls;
+
+		/** @brief method declaration pointer annotations **/
+		method_declarations _method_decls;
+
+		/** @brief constant value annotations **/
+		constant_values _constants;
+	};
+
+	/**
+	 * @brief
+	 *     Checks the semantic validity of the given program.
+	 *
+	 * @param ast
+	 *     program ast
+	 *
+	 * @param pool
+	 *     symbol pool used for identifiers
+	 *
+	 * @return
+	 *     semantic information, including AST annotations
+	 *
+	 */
+	template<typename AllocT>
+	semantic_info check_program(const ast::program& ast,
+							    symbol_pool<AllocT>& pool);
+
+	// FIXME: remove
 	template<typename AllocT>
 	void analyze_ast(const ast::program& ast, symbol_pool<AllocT>& pool);
 }
--- src/core/semantic/semantic.tpp
+++ src/core/semantic/semantic.tpp
@@ -2,6 +2,8 @@
 #error "Never `#include <semantic/analyze.tpp>` directly; `#include <semantic/analyze.hpp>` instead."
 #endif
 
+#include <memory>
+
 #include "semantic/builtins.hpp"
 #include "semantic/constant.hpp"
 #include "semantic/ref_type_analysis.hpp"
@@ -13,6 +15,80 @@
 
 namespace minijava
 {
+	namespace sem
+	{
+		namespace detail
+		{
+			namespace ast = minijava::ast;
+
+			template<typename AllocT>
+			std::unique_ptr<ast::program> make_builtin_ast(
+					symbol_pool<AllocT>& pool) {
+				auto println_arg = std::make_unique<ast::var_decl>(
+						std::make_unique<ast::type>(
+								ast::primitive_type::type_int
+						),
+						pool.normalize(".")
+				);
+				std::vector<std::unique_ptr<ast::var_decl>> args{};
+				args.push_back(std::move(println_arg));
+				auto println = std::make_unique<ast::instance_method>(
+						pool.normalize("println"),
+						std::make_unique<ast::type>(
+								ast::primitive_type::type_void
+						),
+						std::move(args),
+						std::make_unique<ast::block>(
+								std::vector<ast::block_statement>{}
+						)
+				);
+				std::vector<std::unique_ptr<ast::instance_method>> methods{};
+				methods.push_back(std::move(println));
+				auto print_class = std::make_unique<ast::class_declaration>(
+						pool.normalize("java.io.PrintStream"),
+						std::vector<std::unique_ptr<ast::var_decl>>{},
+						std::move(methods),
+						std::vector<std::unique_ptr<ast::main_method>>{}
+				);
+				auto out = std::make_unique<ast::var_decl>(
+						std::make_unique<ast::type>(
+								pool.normalize("java.io.PrintStream")
+						),
+						pool.normalize("out")
+				);
+				std::vector<std::unique_ptr<ast::instance_method>> fields{};
+				fields.push_back(std::move(out));
+				auto system_class = std::make_unique<ast::class_declaration>(
+						pool.normalize("java.lang.System"),
+						std::move(fields),
+						std::vector<std::unique_ptr<ast::instance_method>>{},
+						std::vector<std::unique_ptr<ast::main_method>>{}
+				);
+				auto string_class = std::make_unique<ast::class_declaration>(
+						pool.normalize("java.lang.String"),
+						std::vector<std::unique_ptr<ast::var_decl>>{},
+						std::vector<std::unique_ptr<ast::instance_method>>{},
+						std::vector<std::unique_ptr<ast::main_method>>{}
+				);
+				std::vector<std::unique_ptr<ast::class_declaration>> clazzes{};
+				clazzes.push_back(std::move(print_class));
+				clazzes.push_back(std::move(system_class));
+				clazzes.push_back(std::move(string_class));
+				return std::make_unique<ast::program>(std::move(clazzes));
+			}
+		}
+	}
+
+	template<typename AllocT>
+	semantic_info check_program(const ast::program& ast,
+								symbol_pool<AllocT>& pool)
+	{
+		auto builtin_ast = sem::detail::make_builtin_ast(pool);
+		// FIXME
+		(void) builtin_ast;
+		throw nullptr;
+	}
+
 	template<typename AllocT>
 	void analyze_ast(const ast::program& ast, symbol_pool<AllocT>& pool)
 	{
