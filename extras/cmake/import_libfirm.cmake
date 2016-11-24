1283af0b02432d4edd9284ba1b4b7314cf817e85
SrTobi <code.databyte@gmail.com>
2016-11-24 13:37:49 +0100

    added libfirm as external-project

---
+++ extras/cmake/import_libfirm.cmake
@@ -0,0 +1,21 @@
+function(import_libfirm target)
+	include(ExternalProject)
+	set(install_dir "${CMAKE_BINARY_DIR}/downloads/libFirm/install")
+	ExternalProject_Add(
+		libFirmBuilder
+		GIT_REPOSITORY "http://pp.ipd.kit.edu/git/libfirm.git"
+		#GIT_MODULE CMake
+		PREFIX "${CMAKE_BINARY_DIR}/downloads/libFirm"
+		UPDATE_COMMAND ""
+		CMAKE_ARGS
+			-DCMAKE_INSTALL_PREFIX=${install_dir}
+		LOG_DOWNLOAD ON
+		BUILD_IN_SOURCE ON
+	)
+	#ExternalProject_Get_Property(libFirmBuilder install_dir)
+
+	add_library(${target} INTERFACE)
+	target_link_libraries(${target} INTERFACE "${install_dir}/lib/libfirm.a")
+	target_include_directories(${target} INTERFACE "${install_dir}/include")
+
+endfunction(import_libfirm)
