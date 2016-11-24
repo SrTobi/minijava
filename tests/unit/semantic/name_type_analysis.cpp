818ca8ec02336a5c131c98b3a1fa5b386334db3e
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-11-24 12:33:22 +0100

    Unit tests for name & type analysis 2 / N.
    	* tests/unit/semantic/name_type_analysis.cpp: Add tests for
    	duplicate fields and method declarations and usage of unknown
    	types.

--- tests/unit/semantic/name_type_analysis.cpp
+++ tests/unit/semantic/name_type_analysis.cpp
@@ -46,7 +46,7 @@ BOOST_AUTO_TEST_CASE(shallow_accepts_hello_world)
 }
 
 
-BOOST_AUTO_TEST_CASE(shallow_rejects_bogous_main)
+BOOST_AUTO_TEST_CASE(shallow_rejects_bogus_main)
 {
 	auto pool = minijava::symbol_pool<>{};
 	auto factory = minijava::ast_factory{};
@@ -192,6 +192,112 @@ BOOST_AUTO_TEST_CASE(shallow_rejects_duplicate_fields_of_different_type)
 					)
 				),
 				testaux::make_unique_ptr_vector<ast::instance_method>(),
+				testaux::make_unique_ptr_vector<ast::main_method>(
+					testaux::make_empty_main("main", pool, factory)
+				)
+			)
+		)
+	);
+	auto classes = sem::class_definitions{};
+	auto type_annotations = sem::type_attributes{};
+	sem::extract_type_info(*ast, false, classes);
+	BOOST_REQUIRE_THROW(
+		sem::perform_shallow_type_analysis(*ast, classes, type_annotations),
+		minijava::semantic_error
+	);
+}
+
+
+BOOST_AUTO_TEST_CASE(shallow_rejects_field_of_unknown_type)
+{
+	auto pool = minijava::symbol_pool<>{};
+	auto factory = minijava::ast_factory{};
+	const auto ast = factory.make<ast::program>()(
+		testaux::make_unique_ptr_vector<ast::class_declaration>(
+			factory.make<ast::class_declaration>()(
+				pool.normalize("Test"),
+				testaux::make_unique_ptr_vector<ast::var_decl>(
+					factory.make<ast::var_decl>()(
+						factory.make<ast::type>()(pool.normalize("Foo")),
+						pool.normalize("foo")
+					)
+				),
+				testaux::make_unique_ptr_vector<ast::instance_method>(),
+				testaux::make_unique_ptr_vector<ast::main_method>(
+					testaux::make_empty_main("main", pool, factory)
+				)
+			)
+		)
+	);
+	auto classes = sem::class_definitions{};
+	auto type_annotations = sem::type_attributes{};
+	sem::extract_type_info(*ast, false, classes);
+	BOOST_REQUIRE_THROW(
+		sem::perform_shallow_type_analysis(*ast, classes, type_annotations),
+		minijava::semantic_error
+	);
+}
+
+
+BOOST_AUTO_TEST_CASE(shallow_rejects_method_of_unknown_type)
+{
+	auto pool = minijava::symbol_pool<>{};
+	auto factory = minijava::ast_factory{};
+	const auto ast = factory.make<ast::program>()(
+		testaux::make_unique_ptr_vector<ast::class_declaration>(
+			factory.make<ast::class_declaration>()(
+				pool.normalize("Test"),
+				testaux::make_unique_ptr_vector<ast::var_decl>(),
+				testaux::make_unique_ptr_vector<ast::instance_method>(
+					factory.make<ast::instance_method>()(
+						pool.normalize("getObjectOfUdeclaredType"),
+						factory.make<ast::type>()(pool.normalize("Foo")),
+						testaux::make_unique_ptr_vector<ast::var_decl>(),
+						factory.make<ast::block>()(
+							testaux::make_unique_ptr_vector<ast::block_statement>()
+						)
+					)
+				),
+				testaux::make_unique_ptr_vector<ast::main_method>(
+					testaux::make_empty_main("main", pool, factory)
+				)
+			)
+		)
+	);
+	auto classes = sem::class_definitions{};
+	auto type_annotations = sem::type_attributes{};
+	sem::extract_type_info(*ast, false, classes);
+	BOOST_REQUIRE_THROW(
+		sem::perform_shallow_type_analysis(*ast, classes, type_annotations),
+		minijava::semantic_error
+	);
+}
+
+
+BOOST_AUTO_TEST_CASE(shallow_rejects_parameter_of_unknown_type)
+{
+	auto pool = minijava::symbol_pool<>{};
+	auto factory = minijava::ast_factory{};
+	const auto ast = factory.make<ast::program>()(
+		testaux::make_unique_ptr_vector<ast::class_declaration>(
+			factory.make<ast::class_declaration>()(
+				pool.normalize("Test"),
+				testaux::make_unique_ptr_vector<ast::var_decl>(),
+				testaux::make_unique_ptr_vector<ast::instance_method>(
+					factory.make<ast::instance_method>()(
+						pool.normalize("processObjectOfUdeclaredType"),
+						factory.make<ast::type>()(ast::primitive_type::type_void),
+						testaux::make_unique_ptr_vector<ast::var_decl>(
+							factory.make<ast::var_decl>()(
+								factory.make<ast::type>()(pool.normalize("Foo")),
+								pool.normalize("foo")
+							)
+						),
+						factory.make<ast::block>()(
+							testaux::make_unique_ptr_vector<ast::block_statement>()
+						)
+					)
+				),
 				testaux::make_unique_ptr_vector<ast::main_method>(
 					testaux::make_empty_main("main", pool, factory)
 				)
