ca283162b258c706d96e0e807bd97afe1117fd1d
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-12-08 03:55:51 +0100

    Expose `is_number` and `is_word` functions in `lexer/character.hpp`.
    While these functions are never actually needed in the lexer, there is a
    frequent need for them in functions that are supposed to be only called with
    valid words or numbers and wish to assert these preconditions.  Defining and
    unit-testing these helper functions in the one right place is better than
    redundantly defining poor alternatives in several places or resorting to regex
    and other questionable solutions on an ad hoc basis.
    
    	* src/core/lexer/character.cpp: Add new functions.
    	* src/core/lexer/character.hpp: Likewise.
    	* tests/lexer/character.cpp: Likewise.

--- src/core/lexer/character.cpp
+++ src/core/lexer/character.cpp
@@ -1,3 +1,47 @@
 #include "lexer/character.hpp"
 
-// This file is empty.
+#include <algorithm>
+#include <iterator>
+
+
+namespace minijava
+{
+
+	bool is_number(const std::string& s) noexcept
+	{
+		const auto pred = [](auto&& c){
+			return is_digit(static_cast<unsigned char>(c));
+		};
+		if (s.empty()) {
+			return false;
+		}
+		if (!std::all_of(std::begin(s), std::end(s), pred)) {
+			return false;
+		}
+		if ((s.front() == '0') && (s.length() > 1)) {
+			return false;
+		}
+		return true;
+	}
+
+	bool is_word(const std::string& s) noexcept
+	{
+		const auto pred_head = [](auto&& c){
+			return is_word_head(static_cast<unsigned char>(c));
+		};
+		const auto pred_tail = [](auto&& c){
+			return is_word_tail(static_cast<unsigned char>(c));
+		};
+		if (s.empty()) {
+			return false;
+		}
+		if (!pred_head(s.front())) {
+			return false;
+		}
+		if (!std::all_of(std::next(std::begin(s)), std::end(s), pred_tail)) {
+			return false;
+		}
+		return true;
+	}
+
+}  // namespace minijava
