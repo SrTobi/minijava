65308adc97cce4f532a92c209a283caa95d59a34
Moritz Klammler <moritz.klammler@student.kit.edu>
2016-12-21 16:30:01 +0100

    Sketch for high-level interface of optimization and assembly stage.
    	* CMakeLists.txt: Add new components.
    	* src/core/asm/asm.cpp: New.
    	* src/core/asm/asm.hpp: New.
    	* src/core/opt/opt.cpp: New.
    	* src/core/opt/opt.hpp: New.
    	* tests/unit/asm/asm.cpp: New.
    	* tests/unit/opt/opt.cpp: New.

---
+++ src/core/asm/asm.cpp
@@ -0,0 +1,13 @@
+#include "asm/asm.hpp"
+
+
+namespace minijava
+{
+
+	void assemble(firm_ir& ir, file_output& out)
+	{
+		// TODO: Implement
+		out.print("# TODO: Assemble IRG at address %p\n", &ir);
+	}
+
+}  // namespace minijava
---
+++ src/core/asm/asm.hpp
@@ -0,0 +1,34 @@
+/**
+ * @file asm.hpp
+ *
+ * @brief
+ *     Interface for x64 assembly backend.
+ *
+ */
+
+#pragma once
+
+#include "io/file_output.hpp"
+#include "irg/irg.hpp"
+
+
+namespace minijava
+{
+
+	/**
+	 * @brief
+	 *     Emits x64 assembly for the lowered IRG.
+	 *
+	 * This function performs no optimization.  This has to be done beforehand,
+	 * if desired.
+	 *
+	 * @param ir
+	 *     lowered Firm IRG
+	 *
+	 * @param out
+	 *     file to which the assembly shall be written
+	 *
+	 */
+	void assemble(firm_ir& ir, file_output& out);
+
+}  // namespace minijava
