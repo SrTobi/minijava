2be19ac609118bb34f051b95a00625e507ecd4ab
Philipp Serrer <philipp@serrer.de>
2017-01-01 15:07:46 +0100

    build base structure for creating and running optimizations

--- src/core/opt/opt.cpp
+++ src/core/opt/opt.cpp
@@ -4,9 +4,25 @@
 namespace minijava
 {
 
-	void optimize(firm_ir& /* ir */)
+	std::vector<std::unique_ptr<minijava::opt::optimization>> optimizations;
+
+	void optimize(firm_ir& ir)
 	{
-		// TODO: Implement (in particular, constant propagation)
+		auto changed = false;
+		do
+		{
+			for (auto& opt : optimizations) {
+				changed |= opt->optimize(ir);
+			}
+		} while (changed);
 	}
+
+	void register_optimization(std::unique_ptr<minijava::opt::optimization> opt)
+	{
+		optimizations.push_back(std::move(opt));
+	}
+
+	void register_all_optimizations()
+	{ }
 
 }  // namespace minijava
