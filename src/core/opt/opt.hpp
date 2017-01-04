0f4230c472db1f04600fefbd4ea9d04947abb15b
Philipp Serrer <philipp@serrer.de>
2017-01-04 19:20:37 +0100

    added worklist_optimization class for worklist based optimizations

--- src/core/opt/opt.hpp
+++ src/core/opt/opt.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <queue>
+
 #include "irg/irg.hpp"
 
 
@@ -38,6 +40,36 @@ namespace minijava
 			 */
 			virtual bool optimize(firm_ir& /*ir*/) = 0;
 		};
+
+		using worklist_queue = std::queue<firm::ir_node*>;
+
+		class worklist_optimization : public optimization
+		{
+		protected:
+
+			firm::ir_graph* _irg;
+			bool _changed{};
+
+		public:
+			virtual bool handle(firm::ir_node* node) = 0;
+			virtual void cleanup(firm::ir_node* node);
+
+			virtual bool optimize(firm_ir &ir) override;
+		};
+
+		class worklist
+		{
+		protected:
+			firm::ir_graph* _irg;
+
+		public:
+			worklist(firm::ir_graph* irg);
+
+			void run(worklist_optimization *opt);
+		};
+
+		// functions
+		std::vector<std::pair<firm::ir_node*, int>> get_out_edges_safe(firm::ir_node* node);
 	}
 
 	/**
