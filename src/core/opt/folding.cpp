b555fb38e2de344a1811f5470428070d8b5d4ea8
Philipp Serrer <philipp@serrer.de>
2017-01-23 18:00:41 +0100

    added mux node handling to folding

--- src/core/opt/folding.cpp
+++ src/core/opt/folding.cpp
@@ -1,5 +1,7 @@
 #include "opt/folding.hpp"
 
+#include <iostream>
+
 using namespace minijava::opt;
 
 firm::ir_tarval* get_tarval(firm::ir_node* node, int n)
@@ -22,29 +24,42 @@ bool is_tarval_with_num(firm::ir_tarval* val, long num)
 
 void minijava::opt::folding::cleanup(firm::ir_node* node) {
 	auto opcode = firm::get_irn_opcode(node);
-	if (opcode != firm::iro_Const) {
-		firm::ir_tarval* tv = (firm::ir_tarval*)firm::get_irn_link(node);
-		if (tv && is_tarval_numeric(tv)) {
-			auto new_node = firm::new_r_Const_long(_irg, firm::get_tarval_mode(tv), firm::get_tarval_long(tv));
-			firm::set_irn_link(new_node, tv);
-			// keep memory edges of div/mod nodes
-			if (opcode == firm::iro_Div || opcode == firm::iro_Mod) {
-				for (auto &out_edge : get_out_edges_safe(node)) {
-					if (firm::get_irn_mode(out_edge.first) == firm::mode_M) {
-						for (auto &child_edge : get_out_edges_safe(out_edge.first)) {
-							firm::set_irn_n(child_edge.first, child_edge.second, firm::get_irn_n(node, 0));
-						}
-					} else {
-						firm::exchange(out_edge.first, new_node);
-					}
-				}
-			} else {
-				firm::exchange(node, new_node);
-			}
-			// mark optimization as changed
-			_changed = true;
-		}
+	if (opcode == firm::iro_Const) {
+		return;
 	}
+
+	firm::ir_tarval* tv = (firm::ir_tarval*)firm::get_irn_link(node);
+	if (opcode == firm::iro_Cmp) {
+		// wait for following mux
+	} else if (opcode == firm::iro_Mux) {
+		if (tv) {
+			if (tv == firm::tarval_b_true) {
+				firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_true));
+			} else {
+				firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_false));
+			}
+		}
+	} else if (tv && is_tarval_numeric(tv)) {
+		auto new_node = firm::new_r_Const_long(_irg, firm::get_tarval_mode(tv), firm::get_tarval_long(tv));
+		firm::set_irn_link(new_node, tv);
+		// keep memory edges of div/mod nodes
+		if (opcode == firm::iro_Div || opcode == firm::iro_Mod) {
+			for (auto &out_edge : get_out_edges_safe(node)) {
+				if (firm::get_irn_mode(out_edge.first) == firm::mode_M) {
+					for (auto &child_edge : get_out_edges_safe(out_edge.first)) {
+						firm::set_irn_n(child_edge.first, child_edge.second, firm::get_irn_n(node, 0));
+					}
+				} else {
+					firm::exchange(out_edge.first, new_node);
+				}
+			}
+		} else {
+			firm::exchange(node, new_node);
+		}
+		// mark optimization as changed
+		_changed = true;
+	}
+
 }
 
 bool minijava::opt::folding::handle(firm::ir_node* node) {
@@ -106,6 +121,20 @@ bool minijava::opt::folding::handle(firm::ir_node* node) {
 		auto tv = get_tarval(node, 0);
 		if (is_tarval_numeric(tv)) {
 			ret_tv = firm::new_tarval_from_long(-firm::get_tarval_long(tv), firm::get_irn_mode(node));
+		}
+	} else if (opcode == firm::iro_Cmp) {
+		auto lhs = get_tarval(node, 0);
+		auto rhs = get_tarval(node, 0);
+
+		if (lhs && rhs) {
+			//auto rel = firm::tarval_cmp(lhs, rhs);
+			ret_tv = lhs == rhs ? firm::tarval_b_true : firm::tarval_b_false;
+		}
+	} else if (opcode == firm::iro_Mux) {
+		// pass tv from sel node to mux node
+		auto sel = get_tarval(node, 0);
+		if (sel) {
+			ret_tv = sel;
 		}
 	} else if (opcode == firm::iro_Phi) {
 		auto child_count = firm::get_irn_arity(node);
