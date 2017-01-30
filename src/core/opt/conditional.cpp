67e5cdd70f1e74028793f48b9ab3b899fcf59f29
Philipp Serrer <philipp@serrer.de>
2017-01-30 16:29:11 +0100

    fixed conditional optimization - now should work

--- src/core/opt/conditional.cpp
+++ src/core/opt/conditional.cpp
@@ -10,15 +10,34 @@ void minijava::opt::conditional::cleanup(firm::ir_node* node) {
 		return;
 	}
 
-	firm::ir_tarval* tv = (firm::ir_tarval*)firm::get_irn_link(node);
 	if (opcode == firm::iro_Cmp) {
 		// wait for following mux
 	} else if (opcode == firm::iro_Mux) {
-		if (tv) {
-			if (tv == firm::tarval_b_true) {
-				firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_true));
-			} else {
-				firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_false));
+		firm::ir_tarval* tv = (firm::ir_tarval*)firm::get_irn_link(node);
+		if (tv == firm::tarval_b_true) {
+			firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_true));
+		} else if (tv == firm::tarval_b_false) {
+			firm::exchange(node, firm::get_irn_n(node, firm::n_Mux_false));
+		}
+	} else if (opcode == firm::iro_Phi) {
+		//if (tv) {
+		//	// todo?
+		//}
+	} else if (opcode == firm::iro_Proj) {
+		auto cond = firm::get_Proj_pred(node);
+		if (firm::is_Cond(cond)) {
+			auto block = firm::get_nodes_block(get_irn_n(cond, 0));
+			auto tv = (firm::ir_tarval*)firm::get_irn_link(cond);
+			//(void)block;
+			if (tv == firm::tarval_b_false || tv == firm::tarval_b_true) {
+				firm::ir_node *new_node;
+				if ((firm::get_Proj_num(node) == firm::pn_Cond_true && tv == firm::tarval_b_true) ||
+						(firm::get_Proj_num(node) == firm::pn_Cond_false && tv == firm::tarval_b_false)) {
+					new_node = firm::new_r_Jmp(block);
+				} else {
+					new_node = firm::new_r_Bad(firm::get_irn_irg(node), firm::mode_X);
+				}
+				firm::exchange(node, new_node);
 			}
 		}
 	}
@@ -30,12 +49,23 @@ bool minijava::opt::conditional::handle(firm::ir_node* node) {
 
 	bool changed{false};
 	if (opcode == firm::iro_Cmp) {
-		auto lhs = get_tarval(node, 0);
-		auto rhs = get_tarval(node, 1);
+		auto lhs = firm::get_irn_n(node, 0);
+		auto rhs = firm::get_irn_n(node, 1);
+		if (firm::is_Const(lhs) && firm::is_Const(rhs)) {
+			auto lhs_tv = firm::get_Const_tarval(lhs);
+			auto rhs_tv = firm::get_Const_tarval(rhs);
 
-		if (lhs && rhs) {
-			ret_tv = firm::tarval_cmp(lhs, rhs) & firm::get_Cmp_relation(node) ?
+			ret_tv = firm::tarval_cmp(lhs_tv, rhs_tv) & firm::get_Cmp_relation(node) ?
 			         firm::tarval_b_true : firm::tarval_b_false;
+		} else if (lhs == rhs) {
+			auto rel = firm::get_Cmp_relation(node);
+			if (rel == firm::ir_relation_equal
+				    || rel == firm::ir_relation_greater_equal
+					|| rel == firm::ir_relation_greater_equal) {
+				ret_tv = firm::tarval_b_true;
+			} else if (rel == firm::ir_relation_less_greater) {
+				ret_tv = firm::tarval_b_false;
+			}
 		}
 	} else if (opcode == firm::iro_Mux) {
 		// pass tv from sel node to mux node
@@ -43,6 +73,40 @@ bool minijava::opt::conditional::handle(firm::ir_node* node) {
 		if (sel) {
 			ret_tv = sel;
 			changed = true;
+		}
+	} else if (opcode == firm::iro_Cond) {
+		auto cmp = firm::get_Cond_selector(node);
+		if (firm::is_Cmp(cmp)) {
+			auto tv = (firm::ir_tarval*)firm::get_irn_link(cmp);
+			if (tv) {
+				ret_tv = tv;
+			}
+		}
+	} else if (opcode == firm::iro_Phi) {
+		auto is_bad = false;
+		auto found = false;
+		firm::ir_tarval *val = nullptr;
+		for (int i = 0, n = firm::get_irn_arity(node); i < n; i++) {
+			auto tv = get_tarval(node, i);
+			if (!tv) {
+				found = false;
+				break;
+			} else if (tv == firm::tarval_bad) {
+				is_bad = true;
+				break;
+			} else {
+				if (found == false) {
+					found = true;
+					val = tv;
+				} else if (val != tv) {
+					is_bad = true;
+				}
+			}
+		}
+		if (is_bad) {
+			ret_tv = firm::tarval_bad;
+		} else if (found) {
+			ret_tv = val;
 		}
 	}
 	firm::set_irn_link(node, ret_tv);
