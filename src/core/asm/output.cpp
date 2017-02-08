2dac1ff92ac2beb5c3687922ac1904ee01f12fbc
Moritz Klammler <moritz.klammler@student.kit.edu>
2017-02-08 20:07:21 +0100

    Special-case SETcc instructions in output formatting.

--- src/core/asm/output.cpp
+++ src/core/asm/output.cpp
@@ -157,6 +157,13 @@ namespace minijava
 				case opcode::op_lea:
 					assert(instr.width == bit_width{});
 					return std::make_pair(bit_width::lxiv, bit_width::lxiv);
+				case opcode::op_seta:
+				case opcode::op_setae:
+				case opcode::op_setb:
+				case opcode::op_setbe:
+				case opcode::op_sete:
+				case opcode::op_setne:
+					return std::make_pair(bit_width::viii, bit_width{});
 					// TODO: Add other cases here even though we don't need them?
 				default:
 					return std::make_pair(instr.width, instr.width);
