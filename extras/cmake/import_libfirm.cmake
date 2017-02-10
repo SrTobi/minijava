7d07f312d0e6ec80c587af4af70ff1862313d76d
Moritz Klammler <moritz.klammler@student.kit.edu>
2017-02-10 04:16:55 +0100

    Don't delete Firm's Git info on non-Windows and upgrade to 457ba52f.
    	* README.md: Document behavior.
    	* extras/cmake/import_libfirm.cmake: Don't bother users of POSIX
    	compliant operating systems with the limitations of MS Windows.

--- extras/cmake/import_libfirm.cmake
+++ extras/cmake/import_libfirm.cmake
@@ -4,27 +4,44 @@ else()
 	set(LIBFIRM_GIT_URL "http://pp.ipd.kit.edu/git/libfirm.git")
 endif()
 
+if(DEFINED ENV{LIBFIRM_GIT_REVISION})
+	set(LIBFIRM_GIT_REVISION "$ENV{LIBFIRM_GIT_REVISION}")
+else()
+	set(LIBFIRM_GIT_REVISION "457ba52f6e29cbc2febe58c11b08a59dae5680eb")
+endif()
+
 function(import_libfirm target)
 	include(ExternalProject)
 	set(install_dir "${CMAKE_BINARY_DIR}/downloads/libFirm/install")
-	ExternalProject_Add(
-		libFirmBuilder
-		GIT_REPOSITORY "${LIBFIRM_GIT_URL}"
-		GIT_TAG "5a89d4b83cdf70286bfc338f98527ace8bbc4c60"
-		PREFIX "${CMAKE_BINARY_DIR}/downloads/libFirm"
-		# Skip revision.h generation, since it won't work on Windows
-		PATCH_COMMAND "${CMAKE_COMMAND}" -E remove_directory "${CMAKE_BINARY_DIR}/downloads/libFirm/src/libFirmBuilder/.git"
-		UPDATE_COMMAND ""
-		CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-		           "-DCMAKE_INSTALL_PREFIX=${install_dir}"
-	)
+	if(WIN32)
+		ExternalProject_Add(
+			libFirmBuilder
+			GIT_REPOSITORY "${LIBFIRM_GIT_URL}"
+			GIT_TAG "${LIBFIRM_GIT_REVISION}"
+			PREFIX "${CMAKE_BINARY_DIR}/downloads/libFirm"
+			# Prevent revision.h generation since it won't work on Windows.
+			PATCH_COMMAND "${CMAKE_COMMAND}" -E remove_directory "${CMAKE_BINARY_DIR}/downloads/libFirm/src/libFirmBuilder/.git"
+			UPDATE_COMMAND ""
+			CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+			           "-DCMAKE_INSTALL_PREFIX=${install_dir}"
+		)
+	else()
+		ExternalProject_Add(
+			libFirmBuilder
+			GIT_REPOSITORY "${LIBFIRM_GIT_URL}"
+			GIT_TAG "${LIBFIRM_GIT_REVISION}"
+			PREFIX "${CMAKE_BINARY_DIR}/downloads/libFirm"
+			CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+			           "-DCMAKE_INSTALL_PREFIX=${install_dir}"
+		)
+	endif()
 	add_library(${target} INTERFACE)
 	add_dependencies(${target} libFirmBuilder)
 	target_link_libraries(${target} INTERFACE "${install_dir}/lib/libfirm.a")
-	if(UNIX)
-		target_link_libraries(${target} INTERFACE m)
-	elseif(WIN32)
+	if(WIN32)
 		target_link_libraries(${target} INTERFACE gnurx winmm)
+	else()
+		target_link_libraries(${target} INTERFACE m)
 	endif()
 	target_include_directories(${target} INTERFACE "${install_dir}/include")
 endfunction(import_libfirm)
