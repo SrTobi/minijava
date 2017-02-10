05def975734db7795798376460e98ccf9673d27a
Philipp Serrer <philipp@serrer.de>
2017-02-10 13:14:27 +0100

    [WIP] tailrec

--- src/core/opt/tailrec.cpp
+++ src/core/opt/tailrec.cpp
@@ -9,9 +9,29 @@ using namespace minijava::opt;
 
 namespace /* anonymous */
 {
-	void find_tail_recursion(firm::ir_graph*) {
 
+	void find_tail_recursion(firm::ir_graph *irg)
+	{
+		auto end_block = firm::get_irg_end_block(irg);
+		for (int i = 0, n = firm::get_irn_arity(end_block); i < n; i++) {
+			auto ret = firm::get_irn_n(end_block, i);
+			// shouldn't happen.. just skip
+			if (!firm::is_Return(ret)) continue;
+
+			auto mem = firm::get_Return_mem(ret);
+			if (!firm::is_Proj(mem)) continue;
+			auto call = firm::get_Proj_pred(mem);
+			if (!firm::is_Call(call)) continue;
+			// at this point, we found an call node as return value
+			// check, if we call us and we are in the same node
+			if (firm::get_nodes_block(ret) != firm::get_nodes_block(call)) continue;
+			auto callee = firm::get_Call_callee(call);
+			if (callee == nullptr || firm::get_entity_linktime_irg(callee) != irg) continue;
+
+			std::cout << "found tail rec in " << firm::get_entity_ident(firm::get_irg_entity(irg)) << std::endl;
+		}
 	}
+
 }
 
 bool tailrec::optimize(firm_ir &) {
